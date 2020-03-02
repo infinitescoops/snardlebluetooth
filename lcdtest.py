8227b94e774a990b864f071bf61a7808849e1918
infinitescoops <gojoeyo@yahoo.com>
2020-03-02 09:21:27 -0600

    Create lcdtest.py

---
+++ lcdtest.py
@@ -0,0 +1,37 @@
+import adafruit_ssd1306
+import board
+import busio
+import digitalio
+
+spi = busio.SPI(board.SCK, MOSI=board.MOSI)
+reset_pin = digitalio.DigitalInOut(board.D4)
+cs_pin = digitalio.DigitalInOut(board.D5)
+dc_pin = digitalio.DigitalInOut(board.D6)
+
+oled = adafruit_ssd1306.SSD1306_SPI(128, 64, spi, dc_pin, reset_pin, cs_pin)
+
+oled.fill(0)
+oled.show()
+
+# Create a blank image for drawing
+# Make sure to create image with mode '1' for 1-bit color
+image = Image.new('1', (oled.width, oled.height))
+
+# Get drwaing object to draw on image
+draw = ImageDraw.Draw(image)
+
+
+# Draw a smaller inner rectangle
+draw.rectangle((BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1), outline=0, fill=0)
+ 
+# Load default font.
+font = ImageFont.load_default()
+ 
+# Draw Some Text
+text = "Hello World!"
+(font_width, font_height) = font.getsize(text)
+draw.text((oled.width//2 - font_width//2, oled.height//2 - font_height//2), text, font=font, fill=255)
+ 
+# Display image
+oled.image(image)
+oled.show()
