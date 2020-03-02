c2a6744218a015536e3ecf0dddb20e2ff07400ad
infinitescoops <gojoeyo@yahoo.com>
2020-03-02 09:47:43 -0600

    Update lcdtest.py

--- lcdtest.py
+++ lcdtest.py
@@ -4,9 +4,9 @@ import busio
 import digitalio
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI)
-reset_pin = digitalio.DigitalInOut(24)
-cs_pin = digitalio.DigitalInOut(8)
-dc_pin = digitalio.DigitalInOut(23)
+reset_pin = digitalio.DigitalInOut(board.24)
+cs_pin = digitalio.DigitalInOut(board.8)
+dc_pin = digitalio.DigitalInOut(board.23)
 
 oled = adafruit_ssd1306.SSD1306_SPI(128, 64, spi, dc_pin, reset_pin, cs_pin)
 
